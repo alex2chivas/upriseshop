09d1a8f63c2a05fbb96c481fa4867e806ce3a922
alex2chivas <alex02chivas@yahoo.com>
2021-03-14 23:07:22 -0700

    pc git

--- g.js
+++ g.js
@@ -10,7 +10,7 @@ function cbAdd (err, sdout, sdin) {
     console.log(err)
     return
   }
-  // it worked
+  // it
   console.log('Git add completed')
   exec(`git commit -m "${str}"`, cbCommit)
 }
